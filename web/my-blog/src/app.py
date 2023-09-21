56bef40d65e639e2e8124837c9afd3c4870a50ce
Stephen White <iamsilk3495@gmail.com>
2023-09-21 18:41:07 -0300

    Quick fix

--- web/my-blog/src/app.py
+++ web/my-blog/src/app.py
@@ -2,17 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/show_file_content', methods=['GET'])
-def show_file_content():
+@app.route('/', methods=['GET'])
+def index():
     # Get the 'file' parameter from the query string
-    file_param = request.args.get('file')
-
-    # Assuming you have a directory where these files are stored
-    file_directory = 'files/'  # Update with your file directory path
+    file_param = request.args.get('file', 'files/index.html')
 
     try:
         # Open and read the content of the file
-        with open(file_directory + file_param, 'r') as file:
+        with open(file_param, 'r') as file:
             file_content = file.read()
     except FileNotFoundError:
         return "File not found"
@@ -21,4 +18,4 @@ def show_file_content():
     return render_template('file_template.html', file_content=file_content)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
